2fbf3b3590cb253e6be7cbe267c7aeba46bfd212
YunZi.Song <yoonji5809@gmail.com>
2024-04-16 00:48:05 +0900

    Feat: 앱 컬러 상수 추가

---
+++ lib/common/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColorAccent =  Color.fromARGB(255, 255, 204, 0);
+  static const Color primaryColor10 = Color.fromARGB(255, 255, 223, 163);
+}
